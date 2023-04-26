09608414b7a9bebd627a830b0bcd6d09d13df5b1
maxefimov <efimov.top.academy@gmail.com>
2023-04-26 21:14:24 +0300

    Добавьте файлы проекта.

---
+++ WpfCommandsApp/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCommandsApp
+{
+    
+    public static class MyAppCommands
+    {
+        public static RoutedCommand Exit { set; get; }
+        static MyAppCommands()
+        {
+            Exit = new RoutedCommand("Exit", typeof(MainWindow));
+        }
+    }
+    
+    
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //CommandBinding binding = new();
+            //binding.Command = ApplicationCommands.Help;
+            //binding.Executed += helpExecute;
+            //btnHelp.CommandBindings.Add(binding);
+
+        }
+
+        void helpExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Help for appalication");
+        }
+
+        void exitExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
